9e898cf72cd0d11bd122867cb6ba266fdcb1a7cb
Padma Shree Jena <jenad1177@gmail.com>
2026-01-21 23:21:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 from demographic_data_analyzer import calculate_demographic_data
 
-if __name__ == "__main__":
-    result = calculate_demographic_data()
-    for key, value in result.items():
-        print(f"{key}: {value}")
+calculate_demographic_data()
